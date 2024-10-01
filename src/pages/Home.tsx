41e3950727262bf0e7eba8380c7a763df3579f68
ns <1464521427@qq.com>
2024-10-02 00:03:03 +0800

    feat: 路由测试

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,19 @@
+import { Button } from "antd";
 import React, { FC } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home: FC = () => {
-  return <p>Home</p>;
+  const nav = useNavigate();
+  const clickHandle = () => {
+    nav("login");
+  };
+  return (
+    <div>
+      <p>home</p>
+      <div>
+        <Button onClick={clickHandle}>登录</Button>
+      </div>
+    </div>
+  );
 };
 export default Home;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,17 @@
+import { Button } from "antd";
 import React, { FC } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 const Login: FC = () => {
-  return <p>Login</p>;
+  const nav = useNavigate();
+  return (
+    <div>
+      <p>Login</p>
+      <div>
+        <Button onClick={() => nav(-1)}>返回</Button>
+        <Link to="/register">注册</Link>
+      </div>
+    </div>
+  );
 };
 export default Login;
