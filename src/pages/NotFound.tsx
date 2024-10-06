ef0fdfc4720889bec227fd14a8a65eff77a61057
ns <1464521427@qq.com>
2024-10-06 16:39:52 +0800

    feat:404页面开发

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,19 @@
 import React, { FC } from "react";
+import { Result, Button } from "antd";
+import { useNavigate } from "react-router-dom";
+import { MANAGE_LIST_PATHNAME } from "../router";
 
 const NotFound: FC = () => {
-  return <p>NotFound</p>;
+  const nav = useNavigate();
+  return (
+    <Result
+      status={404}
+      title="404"
+      subTitle="时间纷纷扰扰，我们终会再见"
+      extra={
+        <Button onClick={() => nav(MANAGE_LIST_PATHNAME)}>返回首页</Button>
+      }
+    ></Result>
+  );
 };
 export default NotFound;
